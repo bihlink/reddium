f1c8a16ca01365790c38fe08144f7afa6728e746
bihlink <95501288+bihlink@users.noreply.github.com>
2022-03-17 17:13:03 +0100

    Update constants.tsx

--- functions/constants.tsx
+++ functions/constants.tsx
@@ -1,5 +1,5 @@
-export const DOMAIN = "https://reddium.vercel.app";
-export const REDIRECT_URI = "https://reddium.vercel.app/login";
+export const DOMAIN = "https://remedium.vercel.app";
+export const REDIRECT_URI = "https://remedium.vercel.app/login";
 export const CLIENT_ID = "DT8MZ0k_rAtJ8w";
 
 // export const REDIRECT_URI = "http://localhost:3000/login";
